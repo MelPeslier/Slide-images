ffa2ad755007e5da18a74e65b498952fe5fd41fb
MelPeslier <mel.peslier@gmail.com>
2023-04-19 17:40:23 -0300

    conditions d'actions ok.

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 let imagesTrack = document.getElementById('body');
 
+let posX = 0;
+let toMe = 0;
+let isDown = false
+
 imagesTrack.addEventListener('mousedown', (event) => {
-    console.log("MOUSEDOWN: " + event.clientX);
-    imagesTrack.addEventListener('mousemove', (event2) => {
-        console.log("mousemove : " + event2.clientX)
-    });
+    isDown = true;
+    posX = event.clientX;
 });
+
+imagesTrack.addEventListener('mousemove', (event) => {
+    toMe = event.clientX - posX;
+    seekMovement();
+});
+
+imagesTrack.addEventListener('mouseup',(event) => {
+    isDown = false;
+})
+
+function seekMovement(){
+    if(!isDown){
+        return
+    }
+    console.log('x : ' + toMe);
+    return
+}
 
 // imagesTrack.addEventListener('mouseover')
